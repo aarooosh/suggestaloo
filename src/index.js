eda9710b58343b7505e9e1738cf88fc76e44c63a
Aarush Ghate <aarushghate@Aarushs-MacBook-Air-2.local>
2024-05-27 04:35:10 +0530

    first deploy test

--- src/index.js
+++ src/index.js
@@ -16,16 +16,56 @@ const firebaseConfig = {
     measurementId: "G-SBTKVQXEW5"
   };
 
-initializeApp(firebaseConfig)
-const db = getFirestore()
-const colRef = collection(db, 'data')
-getDocs(colRef)
-  .then((snapshot) => {
-    let data = []
-    snapshot.docs.forEach((doc) => {
-        data.push({...doc.data(),id : doc.id})
+// initializeApp(firebaseConfig)
+// const db = getFirestore()
+// const colRef = collection(db, 'data')
+// getDocs(colRef)
+//   .then((snapshot) => {
+//     let data = []
+//     snapshot.docs.forEach((doc) => {
+//         data.push({...doc.data(),id : doc.id})
+//     })
+//   })
+//   .catch(err =>{
+//     console.error(err)
+//   })
+
+initializeApp(firebaseConfig);
+
+// Get a Firestore Lite instance
+const db = getFirestore();
+
+// Get the form element
+const form = document.getElementById('form-container');
+
+// Add submit event listener to the form
+document.addEventListener('DOMContentLoaded',function(){
+    form.addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    // Get form data
+    const formData = new FormData(form);
+    const nameOfSong = formData.get('item-name');
+    const nameOfCreator = formData.get('creator-name');
+    const recommendedBy = formData.get('recommended-by');
+    const type = formData.get('type');
+    const notes = formData.get('notes');
+
+    try {
+        // Add the form data to Firestore database
+        const docRef = await addDoc(collection(db, "recommendations"), {
+        songName: nameOfSong,
+        creatorName: nameOfCreator,
+        recommendedBy: recommendedBy,
+        type : type,
+        notes: notes
+        });
+        
+        console.log("Document written with ID: ", docRef.id);
+        // Clear the form after submission
+        form.reset();
+    } catch (error) {
+        console.error("Error adding document: ", error);
+    }
     })
-  })
-  .catch(err =>{
-    console.error(err)
-  })
+});
